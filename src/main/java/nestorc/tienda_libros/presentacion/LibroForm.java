121317d35ef7100a6c0e9e9410035b6925f376a0
Neo3691102 <nestor8772@gmail.com>
2025-04-23 17:52:20 -0600

    Se crea la JTable y ademas se insertan los registros directo de la base de datos.

--- src/main/java/nestorc/tienda_libros/presentacion/LibroForm.java
+++ src/main/java/nestorc/tienda_libros/presentacion/LibroForm.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
 import java.awt.*;
 
 @Component
@@ -12,6 +13,8 @@ public class LibroForm extends JFrame {
 
     LibroServicio libroServicio;
     private JPanel panel;
+    private JTable tablaLibros;
+    private DefaultTableModel tablaModeloLibros;
 
 
     @Autowired
@@ -30,5 +33,32 @@ public class LibroForm extends JFrame {
         int x = (tamanioPantalla.width - getWidth()/ 2);
         int y = (tamanioPantalla.height - getHeight()/ 2);
         setLocation(x, y);
+    }
+
+    private void createUIComponents() {
+        // TODO: place custom component creation code here
+        this.tablaModeloLibros = new DefaultTableModel(0, 5);
+        String[] cabeceros = {"Id", "Libro", "Autor", "Precio", "Existencias"};
+        tablaModeloLibros.setColumnIdentifiers(cabeceros);
+        // Instanciar el objeto Jtable
+        this.tablaLibros = new JTable(tablaModeloLibros);
+        listarLibros();
+    }
+
+    private void listarLibros(){
+        // Limpiar tabla
+        tablaModeloLibros.setRowCount(0);
+        //Obtener libros
+        var libros = libroServicio.listarLibros();
+        libros.forEach((libro) -> {
+            Object[] renglonLibro = {
+                    libro.getIdLibro(),
+                    libro.getNombreLibro(),
+                    libro.getAutor(),
+                    libro.getPrecio(),
+                    libro.getExistencias()
+            };
+            this.tablaModeloLibros.addRow(renglonLibro);
+        });
     }
 }
